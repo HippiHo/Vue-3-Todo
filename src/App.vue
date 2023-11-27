c1eba022258ea34fd9bd4ac16109a2b112eebf8b
HippiHo <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>
2023-11-27 19:49:33 +0100

    adds routing and views styling

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import TodoHeader from './components/TodoHeader.vue'
 
 </script>
 
 <template>
+  <TodoHeader/>
   <RouterView />
 </template>
 
